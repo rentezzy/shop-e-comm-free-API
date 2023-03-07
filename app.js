2501d3fc1fb2f54ffe791be83a31fa511fc55678
rentezzy <ren2ren4ik@gmail.com>
2023-03-07 14:01:39 +0200

    Init commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const userRoutes = require("./routes/userRoutes")
+
+const app = express();
+
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+// const userControllers = require("../controllers/user");
+
+// const userRouter = express.Router();
+// userRouter
+//   .router("/api/v1/users")
+//   .get(userControllers.getAllUsers)
+//   .post(userControllers.createNewUser);
